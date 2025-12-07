a2bf4cda6ca34ac1e035efff37129856df0501d8
Gabriel Akara <kaielakara@gmail.com>
2025-12-07 11:57:42 +0100

    Create api for Models
    Creation of api's for the django models was introduced and we are able to request for data which is contained in the database using our api client(py_client)

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,10 @@
 import requests
 
 endpoint = 'http://localhost:8000/api'
+endpoint_two = 'http://localhost:8000/api/model'
 
-get_response = requests.get(endpoint, json={'name' : 'kaiel'}, params={'abc' : '123'})
+# get_response = requests.get(endpoint, json={'name' : 'kaiel'}, params={'abc' : '123'})
+
+get_response = requests.get(endpoint_two)
 
 print(get_response.json())
