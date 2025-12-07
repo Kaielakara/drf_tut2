243a22fb47e36ce679341df6a20a2ebf68336802
Gabriel Akara <kaielakara@gmail.com>
2025-12-07 06:18:46 +0100

    Build basic python client code
    Created a client which sends requests to my django server and get's back json as a response, I also added a new url and views which strictly returns JsonResponse

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path("api", views.api_message, name='api_m')
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, JsonResponse
+import json
 
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+def api_message(request, *args, **kwargs):
+
+    data = {}
+    try:
+        data = json.loads(request.body)
+        
+    except:
+        pass
+    
+    data['headers'] = dict(request.headers)
+    data['params'] = dict(request.GET)
+    return JsonResponse(data)
